8297f297d1a3985895ddb219572f3ea0d162d21f
Jesse Seligman <jesseseligman@gmail.com>
2016-09-12 08:58:03 -0700

    Add redux dependencies

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,29 @@
+// components
 import App from './components/App';
+
+// dependencies
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 injectTapEventPlugin();
 
+const router = <MuiThemeProvider>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+
+      </Route>
+    </Router>
+  </Provider>
+</MuiThemeProvider>
+
+
 ReactDOM.render(
-  <MuiThemeProvider>
-    <App />
-  </MuiThemeProvider>,
+  router,
   document.getElementById('app')
 );
---
+++ app/store.js
@@ -0,0 +1,20 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+import rootReducer from './reducers/index';
+
+const defaultState = {
+
+};
+
+const enhancers = compose(
+  window.devToolsExtension ?
+  window.devToolsExtension() : f => f
+);
+
+const store = createStore(rootReducer, defaultState, enhancers);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
