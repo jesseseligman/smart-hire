4ff89c8410932d918a11f44d4cc11c5b4a2fdd7b
Jesse Seligman <jesseseligman@gmail.com>
2016-09-13 10:49:19 -0700

    Make logo and links responsive in nav. close #10

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
 
 const App = React.createClass({
 
   render() {
 
+    const styleHamburger = {
+      mediumIcon: {
+        width: 48,
+        height: 48,
+      },
+      medium: {
+        width: 96,
+        height: 96,
+        padding: 24,
+      },
+    };
+
     const styleButton = {
       fontWeight: 'lighter'
     };
-    
+
     return <div>
       <nav id="nav-bar">
         <div id="logo">Smart Hire</div>
+
+        <IconButton
+          iconStyle={styleHamburger.mediumIcon}
+          style={styleHamburger.medium}
+        >
+          <NavigationMenu color={'#F9FDFE'} />
+        </IconButton>
 
         <div id="nav-button-container">
           <FlatButton
