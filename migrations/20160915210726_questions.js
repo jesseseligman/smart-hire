c522af9ec898847b343dcfba4a7e1ac78a9a2999
Jesse Seligman <jesseseligman@gmail.com>
2016-09-15 21:12:20 -0700

    Make questions migration close #23

---
+++ migrations/20160915210726_questions.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.up = function(knex) {
+  knex.schema.createTable('questions', (table) => {
+    table.increments();
+    table.integer('job_id')
+      .notNullable()
+      .references('id')
+      .inTable('jobs')
+      .onDelete('CASCADE')
+      .index();
+    table.text('text').notNullable().defaultTo('');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('questions');
+};
