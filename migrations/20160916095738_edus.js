39873116e03f888c62c645a8cdbf6049228d23e9
Jesse Seligman <jesseseligman@gmail.com>
2016-09-16 10:00:41 -0700

    Make edus migration. close #27

---
+++ migrations/20160916095738_edus.js
@@ -0,0 +1,21 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('edus', (table) => {
+    table.increments();
+    table.string('school').notNullable('').defaultTo('');
+    table.string('degree').notNullable('').defaultTo('');
+    table.string('major').notNullable('').defaultTo('');
+    table.string('dates').notNullable('').defaultTo('');
+    table.integer('application_id')
+      .notNullable()
+      .references('id')
+      .inTable('applications')
+      .onDelete('CASCADE')
+      .index();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('edus');
+};
